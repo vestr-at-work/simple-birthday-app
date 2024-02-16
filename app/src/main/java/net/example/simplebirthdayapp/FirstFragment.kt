704fbd48c2ea46aafe08351faebde7625aac5240
Bálek Josef <pepik.balek@seznam.cz>
2024-02-17 00:51:11 +0100

    Add icons (cake, calendar, plus)

--- app/src/main/java/net/example/simplebirthdayapp/FirstFragment.kt
+++ app/src/main/java/net/example/simplebirthdayapp/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import net.example.simplebirthdayapp.databinding.FragmentFirstBinding
 import java.util.Calendar
@@ -15,7 +16,6 @@ import java.util.Calendar
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -24,11 +24,21 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = binding.root
 
+        // Tlačítko zpět
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Nastaví ikonu a zároveň umístění ikony uprostřed
+        (requireActivity() as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.cake)
+
+        // Nastavení titulku action baru na prázdný řetězec
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = ""
+
+        return view
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
