c0b225bfdc58938d60995913e151b6d8cb851784
vestr-at-work <petera-david@seznam.cz>
2024-02-23 23:01:50 +0100

    Add SecondFragment aka ListFragment basic implementation

--- app/src/main/java/net/example/simplebirthdayapp/SecondFragment.kt
+++ app/src/main/java/net/example/simplebirthdayapp/SecondFragment.kt
@@ -12,10 +12,16 @@ import android.widget.TableRow
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import net.example.simplebirthdayapp.data.MonthRecord
 import net.example.simplebirthdayapp.databinding.FragmentSecondBinding
 import net.example.simplebirthdayapp.data.Person
 import net.example.simplebirthdayapp.personStorage.PersonDatabase
 import java.time.LocalDate
+import java.time.Month
 import java.time.temporal.ChronoUnit
 
 class SecondFragment : Fragment() {
@@ -23,6 +29,8 @@ class SecondFragment : Fragment() {
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
     private lateinit var database: PersonDatabase
+
+    private val monthRecordsAdapter = MonthRecordsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,12 +47,22 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val tableLayout = view.findViewById<TableLayout>(R.id.tableLayout)
+        binding.monthCardsList.apply {
+            adapter = monthRecordsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
 
-        database.personDao().getAllPeople().observe(viewLifecycleOwner, Observer {
-            showEvents(tableLayout, it)
+        database.personDao().getAllPeople().observe(viewLifecycleOwner, Observer { it ->
+            // sorted by how many days remain to the next birthday
+            val sortedPeople = it.sortedBy { (365 + ChronoUnit.DAYS.between(LocalDate.now(), LocalDate.of(LocalDate.now().year, it.birthMonth, it.birthDay))) % 365 }
+            val monthRecordList = ArrayList<MonthRecord>()
+
+            for (record in PeopleByMonth(sortedPeople)) {
+                monthRecordList.add(record)
+            }
+
+            monthRecordsAdapter.data = monthRecordList.toList()
         })
-
     }
 
     override fun onDestroyView() {
@@ -52,45 +70,32 @@ class SecondFragment : Fragment() {
         _binding = null
     }
 
-    private fun showEvents(tableLayout: TableLayout, people: List<Person>) {
-        //TODO: get it from database, possible sorted by remaining days
-        val sortedPeople = people.sortedBy { LocalDate.of(2024, it.birthMonth, it.birthDay) }
-        for (person in sortedPeople) {
-            val row = TableRow(requireContext())
+    private class PeopleByMonth(
+        val people: List<Person>
+    ) : Iterable<MonthRecord> {
 
-            val params = TableRow.LayoutParams(
-                TableRow.LayoutParams.WRAP_CONTENT,
-                TableRow.LayoutParams.WRAP_CONTENT
-            )
-            row.layoutParams = params
-            row.gravity = Gravity.CENTER
+        override fun iterator(): Iterator<MonthRecord> {
+            return PeopleByMonthIterator()
+        }
 
-            val monthTextView = TextView(requireContext())
-            monthTextView.text = person.name + " : " + person.birthDay.toString() + ". " +
-                    person.birthMonth.toString() + "."
-            monthTextView.setPadding(16, 16, 32, 16)
-            row.addView(monthTextView)
+        inner class PeopleByMonthIterator : Iterator<MonthRecord> {
+            var index = 0
 
-            val countdownTextView = TextView(requireContext())
-            val today = LocalDate.now()
-            val endDate = LocalDate.of(2024, person.birthMonth, person.birthDay)
-            var daysUntil = ChronoUnit.DAYS.between(today, endDate)
-            if (daysUntil < 0){
-                daysUntil += 365
+            override fun hasNext(): Boolean {
+                return index < people.size
             }
-            countdownTextView.text = "Remaining days: $daysUntil"
-            countdownTextView.setPadding(32, 16, 16, 16)
-            row.addView(countdownTextView)
 
-            /**
-            if (sortedPeople.indexOf(person) % 2 == 0) {
-                row.setBackgroundColor(Color.parseColor("#FFFF00")) // yellow
+            override fun next(): MonthRecord {
+                val peopleInMonth = ArrayList<Person>()
+                val month = people[index].birthMonth
+
+                while (index < people.size && month == people[index].birthMonth) {
+                    peopleInMonth.add(people[index])
+                    index++
+                }
+
+                return MonthRecord(Month.of(month), peopleInMonth.toList())
             }
-            else {
-                row.setBackgroundColor(Color.parseColor("#FFA500")) // orange
-            }
-            */
-            tableLayout.addView(row)
         }
     }
 }
